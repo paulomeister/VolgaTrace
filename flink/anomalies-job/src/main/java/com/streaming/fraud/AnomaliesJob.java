a99584c1ca9c4d7b4a06843bd72f3cccf11eae3a
Jean Paul Delgado <jean.delgado@uao.edu.co>
2026-05-03 23:03:07 -0500

    add CEP for declined-approved pattern anomaly

--- flink/anomalies-job/src/main/java/com/streaming/fraud/AnomaliesJob.java
+++ flink/anomalies-job/src/main/java/com/streaming/fraud/AnomaliesJob.java
@@ -5,14 +5,25 @@ import com.streaming.fraud.functions.AmountAnomalyFn;
 import com.streaming.fraud.model.Alert;
 import com.streaming.fraud.model.Event;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.cep.CEP;
+import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.functions.PatternProcessFunction;
+import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.connector.kafka.source.reader.deserializer.KafkaRecordDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class AnomaliesJob {
 
@@ -40,6 +51,47 @@ public class AnomaliesJob {
         KeyedStream<Event, String> keyedStream = eventStream.keyBy(Event::getCardId);
 
         DataStream<Alert> amountAlerts = keyedStream.process(new AmountAnomalyFn());
+
+        // CEP rule for rejected*3->approved pattern
+        Pattern<Event, ?> declinedPattern = Pattern.<Event>begin("rejects")
+                        .where(new SimpleCondition<Event>() {
+                            @Override
+                            public boolean filter(Event value) throws Exception {
+                                return "REJECTED".equalsIgnoreCase(value.getStatus());
+                            }
+                        })
+                                .times(3).consecutive()
+                        .next("approve")
+                                .where(new SimpleCondition<Event>() {
+                                    @Override
+                                    public boolean filter(Event value) throws Exception {
+                                        return "APPROVED".equalsIgnoreCase(value.getStatus());
+                                    }
+                                })
+                                        .within(Time.minutes(10));
+
+        // subscribe pattern to keyed stream
+        PatternStream<Event> patternStream = CEP.pattern(keyedStream, declinedPattern);
+
+        // search for coincidences and trigger alerts
+        DataStream<Alert> cepAlerts = patternStream.process(
+                new PatternProcessFunction<Event, Alert>() {
+                    @Override
+                    public void processMatch(Map<String, List<Event>> match, Context ctx, Collector<Alert> out) throws Exception {
+
+                        Event approveEvent = match.get("approve").get(0);
+
+                        out.collect(new Alert(
+                                UUID.randomUUID().toString(),
+                                approveEvent.getCardId(),
+                                "DECLINED_PATTERN",
+                                Instant.now(),
+                                approveEvent.getTransactionId()
+                        ));
+
+                    }
+                }
+        );
 
         env.execute("Fraud Detection, Anomalies Job"); // this throws Exception
 
